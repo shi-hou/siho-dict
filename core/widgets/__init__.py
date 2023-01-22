7554020770528f19fd53f871acfed7a7e0587854
烽 <feng5227@foxmail.com>
2023-01-22 13:33:09 +0800

    移除toast, 显示有Bug

--- core/widgets/__init__.py
+++ core/widgets/__init__.py
@@ -1,7 +1,6 @@
 from .IPage import IPage
 from .IMenu import IMenu
 from .ISwitch import ISwitch
-from .IToast import IToast
 from .IGroup import IGroup
 from .ILineEdit import ILineEdit
 from .BaseWindow import BaseWindow
